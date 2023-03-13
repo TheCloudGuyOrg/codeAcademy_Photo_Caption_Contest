db5df6b006045559fba2d2adf86499e1403aea99
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-03-13 14:41:10 -0600

    Adding DB Test

---
+++ test/index_test.js
@@ -0,0 +1,21 @@
+//Import Modules
+const assert = require('assert')
+
+
+//Test DB Connection
+describe('Test Database Connection', () => {
+  it('Connection Successful', async () => {
+      
+    // Setup
+    expected = true
+
+    // Exercise
+    result = true
+
+    // Validate
+    assert.equal(result, expected)
+
+    // Teardown
+
+  })
+})
