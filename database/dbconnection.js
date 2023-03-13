85dba420019b0f4667fee23b809e83876588bb60
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-03-13 14:42:20 -0600

    Adding DB Connection Logit

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -5,18 +5,18 @@ const connectionString = `postgresql://${DB_USER}@${DB_HOST}:${PORT}/${DB_DATABA
 const sequelize = new Sequelize(connectionString); 
 
 
-  const testConnection = async (request, response) => {
+const testConnection = async (request, response) => {
     
-    try {
-        await sequelize.authenticate();
-            console.log('Connection has been established successfully.');
-      } 
-      catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-  };
+  try {
+    await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } 
+    catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+};
 
 testConnection()
 
 //Export DB Connection
-module.exports = { sequelize }
+module.exports = { sequelize, testConnection }
