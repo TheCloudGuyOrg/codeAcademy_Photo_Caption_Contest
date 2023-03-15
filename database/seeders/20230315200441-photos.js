202af659ad5f5be31383292db71ba942cf9245d4
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-03-15 14:06:04 -0600

    Building sequelize seeders

---
+++ database/seeders/20230315200441-photos.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
+
+/*
+file:///Users/dercox/Documents/Code/Code_Academy/Backend_Engineer/codeAcademy_Photo_Caption_Contest/public/images/bryce_canyon.jpg
+
+file:///Users/dercox/Documents/Code/Code_Academy/Backend_Engineer/codeAcademy_Photo_Caption_Contest/public/images/bryce_canyon2.jpg
+
+file:///Users/dercox/Documents/Code/Code_Academy/Backend_Engineer/codeAcademy_Photo_Caption_Contest/public/images/cottonwood_pass.jpg
+
+file:///Users/dercox/Documents/Code/Code_Academy/Backend_Engineer/codeAcademy_Photo_Caption_Contest/public/images/daisy_echo_lake.jpg
+*/
