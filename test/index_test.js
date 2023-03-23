5b6cbf2c34dc163c7bb207220ced56bc0926952e
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-03-23 15:02:35 -0600

    Setup tests for getPhotoById Failing

--- test/index_test.js
+++ test/index_test.js
@@ -8,18 +8,18 @@ const app = require('../index.js')
 describe('GET /routes/photos', () => {
   it('status_code: 200', async () => {
       
-      // Setup
-      const excerciseUrl = '/route/photos'
-      const expected = 200
+    // Setup
+    const excerciseUrl = '/route/photos'
+    const expected = 200
 
-      // Exercise
-      const response = await request(app)
-          .get(excerciseUrl)
+    // Exercise
+    const response = await request(app)
+      .get(excerciseUrl)
 
-      const result = response.status
+    const result = response.status
 
-      // Verify
-      assert.equal(result, expected)
+    // Verify
+    assert.equal(result, expected)
   })
 
   it('Status: OK', async () => {
@@ -42,7 +42,14 @@ describe('GET /routes/photos', () => {
         
     // Setup
     const excerciseUrl = '/route/photos'
+    const setupUrl = ''
+    const teardownUrl = ''
     const expected = 'Bryce Canyon 1'
+
+    await request(app)
+      .post(setupUrl)
+
+    console.log('SETUP / TEARDOWN test entry')
 
     // Exercise
     const response = await request(app)
@@ -52,13 +59,108 @@ describe('GET /routes/photos', () => {
 
     // Verify
     assert.equal(result, expected)
+
+    //Teardown
+    await request(app)
+      .delete(teardownUrl)
+    
+    console.log('SETUP / TEARDOWN test entry')
+  })
+});
+
+//Test: GET /routes/photos/:id
+describe('GET /routes/photos/:id', () => {
+  it('status_code: 200', async () => {
+      
+    // Setup
+    const excerciseUrl = '/route/photos/9999'
+    const setupUrl = ''
+    const teardownUrl = ''
+    const expected = 200
+      
+    await request(app)
+      .post(setupUrl)
+
+    console.log('SETUP / TEARDOWN test entry')
+
+    // Exercise
+    const response = await request(app)
+      .get(excerciseUrl)
+
+    const result = response.status
+
+     // Verify
+    assert.equal(result, expected)
+
+    //Teardown
+    await request(app)
+      .delete(teardownUrl)
+    
+    console.log('SETUP / TEARDOWN test entry')
+  })
+
+  it('Status: OK', async () => {
+        
+    // Setup
+    const excerciseUrl = '/route/photos/9999'
+    const setupUrl = ''
+    const teardownUrl = ''
+    const expected = 'OK'
+
+    await request(app)
+      .post(setupUrl)
+
+    console.log('SETUP / TEARDOWN test entry')
+
+    // Exercise
+    const response = await request(app)
+        .get(excerciseUrl)
+
+    const result = response.res.statusMessage
+
+    // Verify
+    assert.equal(result, expected)
+
+    //Teardown
+    await request(app)
+      .delete(teardownUrl)
+      
+    console.log('SETUP / TEARDOWN test entry')
+  })
+
+  it('Validate: Database Retrieval', async () => {
+        
+    // Setup
+    const excerciseUrl = '/route/photos/9999'
+    const setupUrl = ''
+    const teardownUrl = ''
+    const expected = 'Test_Photo'
+
+    await request(app)
+      .post(setupUrl)
+
+    console.log('SETUP / TEARDOWN test entry')
+
+    // Exercise
+    const response = await request(app)
+      .get(excerciseUrl)
+
+    const result = response.body[0].name
+
+    // Verify
+    assert.equal(result, expected)
+
+    //Teardown
+    await request(app)
+      .delete(teardownUrl)
+        
+    console.log('SETUP / TEARDOWN test entry')
   })
 });
 
 
 /*
 Add tests for the following
-- getPhotosById
 - addPhotos
 - updatePhotos
 - deletePhotos
