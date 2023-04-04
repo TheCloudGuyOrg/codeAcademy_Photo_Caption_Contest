4a927afbf52a73a881fd7a22e3fa46be49f5cc7d
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-04-04 15:52:07 -0600

    Fixing passport local strategy

--- auth/passport.js
+++ auth/passport.js
@@ -14,63 +14,31 @@ const getUserByName = async (username) => {
   })
 }
 
+
 passport.use(new LocalStrategy(
   function (username, password, done) {
     getUserByName(username)
       .then((user) => {
   
-        if(!user) {
+        if(user[0] == null) {
           return done(null, false)
         };
   
         const foundMatch = bcrypt.compare(password,user[0].dataValues.password);
   
-        if(!foundMatch) {
-          return done(null, false)
-      }
-  
-        return done(null, user)
-  
+        foundMatch.then((match) => {
+          if(match == false) {
+            return done(null, false)
+          } 
+
+          return done(null, user)
+        })
       })
       .catch((error) => {
         console.log(error)
       })
-    } 
-))
-
-
-/*
-const validateUser = (username, password, done) => {
-  getUserByName(username)
-    .then((user) => {
-      dbUser = user[0].dataValues.name
-      dbPassword = user[0].dataValues.password
-      console.log(dbUser, dbPassword)
-
-      if(!user) {
-        return done(null, false)
-      };
-
-      const foundMatch = bcrypt.compare(password,user[0].dataValues.password);
-
-      if(!foundMatch) {
-        return done(null, false)
-    }
-
-      foundMatch.then((match) => {console.log(match)})
-
-      return done(null, user)
-
     })
-    .catch((error) => {
-      console.log(error)
-    })
-  } 
-
-validateUser('admin', 'admin')
-validateUser('dercox', 'admin')
-*/
-
+)
 
 passport.serializeUser((id, done) => {
   done(null, id);
